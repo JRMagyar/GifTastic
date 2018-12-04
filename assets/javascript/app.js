4649caa25dc961e72a68d7d74ec352fdea8108b6
Julia Magyar <magyar.juliar@gmail.com>
2018-12-03 19:54:33 -0500

    added ratings and fav button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,19 +26,36 @@ function renderGifs(){
     method: "GET"
     })
         .then(function(response){
+            console.log(response)
             $("#gif-holder").empty();
             for(i=0; i < response.data.length; i++){
                 var animate = response.data[i].images.original.url;
                 var static = response.data[i].images.original_still.url;
                 var img = $("<img>")
+                var rated = $("<p>")
+                var fav = $("<img>")
 
+                rated.html("Rating: " + response.data[i].rating + "<br>")
+                rated.attr("class", "rating")
+
+                fav.attr("class", "fav-btn")
+                fav.addClass("btn")
+                fav.attr("data-faved", "no")
+                fav.attr("src", "assets/images/unliked.png")
+                fav.attr("value", [i])
+                             
                 img.attr("data-animate", animate);
                 img.attr("data-static", static);
-                img.attr("class", "gifs")
-                img.attr("data-status", "static")
-                img.attr("src", static)
-
-                $("#gif-holder").append(img)
+                img.attr("class", "gifs");
+                img.attr("data-status", "static");
+                img.attr("src", static);
+                img.attr("id", "img-" + [i])
+                
+                rated.prepend(fav)
+                rated.append(img)
+                $("#gif-holder").append(rated)
+                // $("#gif-holder").append(img)
+                
             }
         })
 }
@@ -65,6 +82,27 @@ $(document).on("click", ".gifs", function(){
         $(this).attr("src", $(this).attr("data-static"))
         $(this).attr("data-status", "static")
     }
+})
+
+$(document).on("click", ".fav-btn", function(){
+    if($(this).attr("data-faved") == "no"){
+    var imgNum = $(this).attr("value");
+    var copyImg = $("#img-" + imgNum);
+    var newImg = $("<img>");
+    
+    newImg.attr("data-animate", copyImg.attr("data-animate"));
+    newImg.attr("data-static", copyImg.attr("data-static"));
+    newImg.attr("class", "gifs");
+    newImg.attr("data-status", "static");
+    newImg.attr("src", copyImg.attr("data-static"));
+    newImg.addClass("copied");
+
+    $("#fav-holder").append(newImg);
+
+    $(this).attr("data-faved", "yes");
+    $(this).attr("src", "assets/images/liked.png")
+    }
+    
 })
 
 renderButtons();
